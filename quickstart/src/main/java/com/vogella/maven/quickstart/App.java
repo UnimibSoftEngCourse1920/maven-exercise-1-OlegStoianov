fa234724d636b155dad4cfcf14cd5d1dd69d6689
Oleg Stoianov <o.stoianov@campus.unimib.it>
2019-11-15 18:27:40 +0100

    Aggiunta dipendenza GSON e creazione di oggetto GSON in Main

--- quickstart/src/main/java/com/vogella/maven/quickstart/App.java
+++ quickstart/src/main/java/com/vogella/maven/quickstart/App.java
@@ -4,10 +4,13 @@ package com.vogella.maven.quickstart;
  * Hello world!
  *
  */
-public class App 
+import com.google.gson.Gson;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
     }
 }
